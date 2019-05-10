2cd4364da5b027947d3006fd913ef78290b399ea
Eniga Ahiante <enigaahiante@aol.com>
2019-05-10 14:53:20 +0100

    added controller for frequencies and subscriptions

---
+++ HackathonAPI/Models/Frequencies.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HackathonAPI.Models
+{
+    public class Frequencies
+    {
+        public Frequencies()
+        {
+        }
+    }
+}
---
+++ HackathonAPI/Repositories/FrequenciesRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HackathonAPI.Repositories
+{
+    public class FrequenciesRepository
+    {
+        public FrequenciesRepository()
+        {
+        }
+    }
+}
